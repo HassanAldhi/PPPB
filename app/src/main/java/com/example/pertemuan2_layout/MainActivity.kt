1dd2c8ff115efcb950f4b7a4aef72ca3558ac2b2
HassanAldhi <Hassanaldhi@gmail.com>
2023-09-26 22:10:27 +0700

    update tugas pertemuan 6

--- app/src/main/java/com/example/pertemuan2_layout/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan2_layout/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.pertemuan2_layout
 
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -15,36 +15,69 @@ import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import com.example.pertemuan2_layout.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
-    private val newSession = false
-    companion object {
-        const val EXTRA_NAME = "extra_name";
-        const val EXTRA_MAIL = "extra_mail";
-        const val EXTRA_PHONE = "extra_phone";
-    }
+    private var selectedDate = ""
+    private var selectedTime = ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding) {
+        val status = arrayOf("Hadir Tepat Waktu", "Sakit",
+            "Terlambat", "Izin")
 
-            btnRegister.setOnClickListener {
-                val intentToSecondActivity =
-                    Intent(
-                        this@MainActivity,
-                        SecondActivity::class.java
-                    )
-                val name = edtUsername.text.toString()
-                val mail = edtEmail.text.toString()
-                val phone = edtPhone.text.toString()
-                intentToSecondActivity.putExtra(EXTRA_NAME, name)
-                intentToSecondActivity.putExtra(EXTRA_MAIL, mail)
-                intentToSecondActivity.putExtra(EXTRA_PHONE, phone)
-                startActivity(intentToSecondActivity)
+        with (binding) {
+//          Menghubungkan data dengan tampilan Spinner
+            val adapterStatus = ArrayAdapter<String>(
+                this@MainActivity,
+                android.R.layout.simple_spinner_item,
+                status
+            )
+//          Menghubungkan adapter ke Spinner
+            spinnerStatus.adapter = adapterStatus
+
+            // get selected item
+//          Mengatur tindakan saat memilih salah satu item dari Spinner
+            spinnerStatus.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View, position: Int, id: Long
+                    ) {
+                        if (status[position] != "Hadir Tepat Waktu") {
+                            edtKeterangan.visibility = View.VISIBLE
+                        }
+                        else {
+                            edtKeterangan.visibility = View.INVISIBLE
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+                }
+
+            // mengambil tanggal (date) dan waktu (time)
+            datePicker.init(
+                // Menyesuaikan tanggal
+                datePicker.year,
+                datePicker.month,
+                datePicker.dayOfMonth
+            ) { _, year, month, dayOfMonth ->
+//              formating tanggal
+                selectedDate = "$dayOfMonth/${month + 1}/$year"
+            }
+
+            // setontime punya 3 respond yaitu view, hourofDay, dan minute
+            // biasanya view diganti dengan _ karena tidak diubah atau tidak dipakai
+            timePickerView.setOnTimeChangedListener { _, hourOfDay, minute ->
+                selectedTime = "$hourOfDay:$minute"
+            }
+            btnSubmit.setOnClickListener {
+                val message = "Presensi Berhasil $selectedDate jam $selectedTime"
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
             }
         }
     }
